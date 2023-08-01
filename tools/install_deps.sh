19dca7e89e6e82afd3e1863735ad765d7abf7a73
Scott McKay <skottmckay@gmail.com>
2023-08-02 08:51:59 +1000

    Try with just openssl-dev. Most likely will need to update the cmake path to use curl from the triton client install if find_package fails as expected.

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -4,13 +4,13 @@ if [[ "$OCOS_ENABLE_AZURE" == "1" ]]
 then
    if [[ "$1" == "many64" ]]; then
      # yum -y install openssl openssl-devel wget && wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz && tar zxvf v1.1.0.tar.gz && cd rapidjson-1.1.0 && mkdir build && cd build && cmake .. && cmake --install . && cd ../.. && git clone https://github.com/triton-inference-server/client.git --branch r23.05 ~/client && ln -s ~/client/src/c++/library/libhttpclient.ldscript /usr/lib64/libhttpclient.ldscript
-     yum -y install libcurl4-openssl-dev
+     yum -y install openssl-devel
    elif [[ "$1" == "many86" ]]; then
      # yum -y install openssl openssl-devel wget && wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz && tar zxvf v1.1.0.tar.gz && cd rapidjson-1.1.0 && mkdir build && cd build && cmake .. && cmake --install . && cd ../.. && git clone https://github.com/triton-inference-server/client.git --branch r23.05 ~/client && ln -s ~/client/src/c++/library/libhttpclient.ldscript /opt/rh/devtoolset-10/root/usr/lib/libhttpclient.ldscript
-     yum -y install libcurl4-openssl-dev
+     yum -y install openssl-devel
    else # for musllinux
      # Who uses this??? 
      # apk add openssl-dev wget && wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz && tar zxvf v1.1.0.tar.gz && cd rapidjson-1.1.0 && mkdir build && cd build && cmake .. && cmake --install . && cd ../.. && git clone https://github.com/triton-inference-server/client.git --branch r23.05 ~/client && ln -s ~/client/src/c++/library/libhttpclient.ldscript /usr/lib/libhttpclient.ldscript
-     apk add libcurl4-openssl-dev
+     apk add openssl-devel
    fi
 fi
