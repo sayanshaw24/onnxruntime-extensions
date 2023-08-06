0a1ee3a8f1d2470ee470db4dd849597d34d49f67
Scott McKay <Scott.McKay@microsoft.com>
2023-08-06 18:25:00 +1000

    Debug windows issue

--- tools/install_deps.bat
+++ tools/install_deps.bat
@@ -1,6 +1,7 @@
 @echo off
 if "%1" == "install" (
-  if not exist "%ProgramFiles%\Miniconda3\python3.exe" (
+  dir "%ProgramFiles%"
+  if not exist "%ProgramFiles%\Miniconda3\python3.exe" and exist "%ProgramFiles%\Miniconda3\python.exe" (
     mklink "%ProgramFiles%\Miniconda3\python3.exe" "%ProgramFiles%\Miniconda3\python.exe"
   )
 )
