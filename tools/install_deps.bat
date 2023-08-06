df33cda73a644bd5140c848377f7d660e016175b
Scott McKay <Scott.McKay@microsoft.com>
2023-08-06 18:29:05 +1000

    fix AND

--- tools/install_deps.bat
+++ tools/install_deps.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%1" == "install" (
-  dir "%ProgramFiles%"
-  if not exist "%ProgramFiles%\Miniconda3\python3.exe" and exist "%ProgramFiles%\Miniconda3\python.exe" (
+  REM This doesn't work on a CI where Miniconda isn't used.
+  if not exist "%ProgramFiles%\Miniconda3\python3.exe" if exist "%ProgramFiles%\Miniconda3\python.exe" (
     mklink "%ProgramFiles%\Miniconda3\python3.exe" "%ProgramFiles%\Miniconda3\python.exe"
   )
 )
