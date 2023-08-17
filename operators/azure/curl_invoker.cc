baa71d719aed2daf63f0871f413e3d392166e450
Scott McKay <skottmckay@gmail.com>
2023-08-17 16:18:51 +1000

    Set CA path and change openssl hashing to use md5

--- operators/azure/curl_invoker.cc
+++ operators/azure/curl_invoker.cc
@@ -38,6 +38,15 @@ CurlHandler::CurlHandler(WriteCallBack callback) : curl_(curl_easy_init(), curl_
   curl_easy_setopt(curl, CURLOPT_FTP_SKIP_PASV_IP, 1L);      // what does this have to do with http requests?
   curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+  
+//#if defined(__ANDROID__)
+  curl_easy_setopt(curl, CURLOPT_CAPATH, "/system/etc/security/cacerts");
+  curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+// #endif
+
+  // TEMP
+  // https://stackoverflow.com/questions/25253823/how-to-make-ssl-peer-verify-work-on-android
+  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
   // should this be configured via a node attribute? different endpoints may have different timeouts
   curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15);
